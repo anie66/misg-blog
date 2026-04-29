e56310ac21f98f062f05917c6663bbe6fd1377be
anie66 <info@maidinstgervais.com>
2026-04-29 07:44:25 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-1YK7BVFW79"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1YK7BVFW79');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
